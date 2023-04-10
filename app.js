37226cd39f05a7fd858ed336f235f80199b903df
gabriel M <gabriel_jm_3@hotmail.com>
2023-04-10 18:06:28 -0300

    my fukink first commit

--- app.js
+++ app.js
@@ -36,15 +36,19 @@ function myFunction() {
     }
   }
 
+  const flipCards = document.querySelectorAll('.flip-card');
 
-  const flipCardBack = document.querySelector('.flip-card-back');
-
-  flipCardBack.addEventListener('click', function() {
-    const flipCardInner = flipCardBack.parentNode;
-    flipCardInner.style.transform = 'rotateY(0deg)';
-  });
-  const btnMasInfo = document.querySelector('.button-card');
-  btnMasInfo.addEventListener('click', function() {
-    const cardInner = document.querySelector('.flip-card-inner');
-    cardInner.style.transform = 'rotateY(180deg)';
+  flipCards.forEach((flipCard) => {
+    const flipCardBack = flipCard.querySelector('.flip-card-back');
+  
+    flipCardBack.addEventListener('click', function() {
+      const flipCardInner = flipCardBack.parentNode;
+      flipCardInner.style.transform = 'rotateY(0deg)';
+    });
+  
+    const btnMasInfo = flipCard.querySelector('.button-card');
+    btnMasInfo.addEventListener('click', function() {
+      const cardInner = flipCard.querySelector('.flip-card-inner');
+      cardInner.style.transform = 'rotateY(180deg)';
+    });
   });
