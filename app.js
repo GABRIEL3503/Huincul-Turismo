08590ab8f09943aa58b070bff19768eae4129ea6
gabriel M <gabriel_jm_3@hotmail.com>
2023-04-18 19:30:05 -0300

    my fukink first commit

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+var coll = document.getElementsByClassName("collapsible");
+var i;
 
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
 
 function myFunction() {
     var x = document.getElementById("myNavbar");
@@ -29,4 +42,20 @@ function myFunction() {
       flipCardInner.style.transform = 'rotateY(180deg)';
     });
   });
+  const summaryElement = document.getElementById('summary-element');
+
+summaryElement.addEventListener('click', function() {
+  // Obtener el elemento de detalles
+  const detailsElement = this.parentNode;
   
+  // Obtener el estado actual de los detalles (abierto o cerrado)
+  const isOpen = detailsElement.hasAttribute('open');
+  
+  if (isOpen) {
+    // Si los detalles están abiertos, retrasar el cierre por 500ms (medio segundo)
+    setTimeout(function() {
+      detailsElement.removeAttribute('open');
+    }, 500);
+  }
+});
+
