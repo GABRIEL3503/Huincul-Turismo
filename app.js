baa9b0c65e1dff586c40f4bbff5d729b7711d4d1
gabriel M <gabriel_jm_3@hotmail.com>
2023-04-21 14:45:14 -0300

    my fukink first commit

--- app.js
+++ app.js
@@ -32,3 +32,11 @@ flipCards.forEach((flipCard) => {
     flipCardInner.style.transform = 'rotateY(180deg)';
   });
 });
+function myFunction() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
