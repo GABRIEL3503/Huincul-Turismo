019effbdd4975c2ec339ad40fe4017048017a7d7
gabriel M <gabriel_jm_3@hotmail.com>
2025-02-24 14:46:31 -0300

    gabo commit

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use(session({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 
 app.use(express.static('public'));
 app.use('/uploads', express.static('uploads')); // Servir archivos subidos
